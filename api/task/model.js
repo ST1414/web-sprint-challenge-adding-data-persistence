c392e1031ab58799089d5336d2608083750f4b8f
ST1414 <sterry1414@gmail.com>
2021-12-10 11:43:26 -0800

    Task router and model completed

--- api/task/model.js
+++ api/task/model.js
@@ -1,10 +1,37 @@
 // ***** TASKS MODEL *****
+const db = require('../../data/dbConfig');
 
-// - [ ] `[POST] /api/tasks`
-//   - Even though `task_completed` is stored as an integer, the API uses booleans when interacting with the client
-//   - Example of response body: `{"task_id":1,"task_description":"baz","task_notes":null,"task_completed":false,"project_id:1}`
+async function getAllTasks () {
+    // - [ ] `[GET] /api/tasks`
+    //   - Even though `task_completed` is stored as an integer, the API uses booleans when interacting with the client
+    //   - Each task must include `project_name` and `project_description`
+    //   - Example of response body: `[{"task_id":1,"task_description":"baz","task_notes":null,"task_completed":false,"project_name:"bar","project_description":null}]`
+    const rows = await db('tasks');
+    const result = rows.map( task => {
+        if (task.task_completed === 0){
+            task.task_completed = false;
+        } else {
+            task.task_completed = true;
+        }
+        return task;
+    });
+    return result;
 
-// - [ ] `[GET] /api/tasks`
-//   - Even though `task_completed` is stored as an integer, the API uses booleans when interacting with the client
-//   - Each task must include `project_name` and `project_description`
-//   - Example of response body: `[{"task_id":1,"task_description":"baz","task_notes":null,"task_completed":false,"project_name:"bar","project_description":null}]`
+}
+
+async function createTask (task) {
+    const newTaskId = await db('tasks').insert(task);
+    const newTask = getTaskById(newTaskId);
+    return newTask;
+}
+
+async function getTaskById (id) { 
+    const task = await db('tasks').where('task_id', id).first();
+    task.task_completed === 0 ? task.task_completed = false : task.task_completed = true;
+    return task;
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,9 +1,22 @@
 // ***** TASKS ROUTER *****
 const express = require('express');
 const router = express.Router();
+const Task = require('./model');
 
-router.get('/', (req, res) => {
-    res.send('<h1>Tasks Router</h1>')
+router.get('/', (req, res, next) => {
+    Task.getAllTasks()
+        .then( response => {
+            res.json(response)
+        })
+        .catch( next )
+})
+
+router.post('/', (req, res, next) => {
+    Task.createTask(req.body)
+        .then( response => {
+            res.json(response);
+        })
+        .catch( next );
 })
 
 router.get('*', (req, res) => {
