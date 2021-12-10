d1631a15a10ebbc8dff111893c17c1459aece886
ST1414 <sterry1414@gmail.com>
2021-12-10 08:53:21 -0800

    server.js updated with all router imports and home url

--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
 // build your server here and require it from index.js
 const express = require('express');
-const router = require('./')
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
 
 const server = express();
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('<h1>Welcome to the 4.2.5 Sprint Challenge</h1>')
+})
+server.use('/api/project', projectRouter);
+server.use('/api/resource', resourceRouter);
+server.use('/api/task', taskRouter);
+
 
